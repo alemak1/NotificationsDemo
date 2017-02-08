a82861f910073700fd2a5d80196fb6aa283f19df
yongrenwang <czie.alexmak@gmail.com>
2017-02-08 16:21:05 +0800

    Implemented a scheduleNotificationMethod that customizes content of notification, creates a request with the content and a trigger of type UNTimeIntervalNotificationTrigger, and adds the request to the notification center

--- NotificationsDemo/NotificationsDemo/ViewController.swift
+++ NotificationsDemo/NotificationsDemo/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scheduleNotification()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func scheduleNotification(){
+        let content = UNMutableNotificationContent()
+        content.title = "10 second notification demo"
+        content.subtitle = "From Changzhou Panda"
+        content.body =  "Notification after 10 seconds - Your alert is ready"
+    
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "10 second notificaiton", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
-
+    
+  
 
 }
 
